a4691bfb266d5bd7b7b3aae37d1a388524c233e0
Rodrigo Silvério <silverio_tenor@outlook.com>
2026-09-11 20:13:25 -0300

    fix(test): US-908 adjust server log and add truncate tables for tests

--- infrastructure/src/test/java/org/libraryexpress/infrastructure/api/e2e/E2EBaseConfig.java
+++ infrastructure/src/test/java/org/libraryexpress/infrastructure/api/e2e/E2EBaseConfig.java
@@ -3,9 +3,15 @@ package org.libraryexpress.infrastructure.api.e2e;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.libraryexpress.domain.core.logging.CustomLogger;
+import org.libraryexpress.domain.core.logging.CustomLoggerFactory;
+import org.libraryexpress.infrastructure.api.server.EmbeddedHttpServer;
 import org.libraryexpress.infrastructure.config.AppBootstrapper;
+import org.libraryexpress.infrastructure.config.logging.Slf4jLoggerAdapter;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -17,6 +23,13 @@ public abstract class E2EBaseConfig {
     protected static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17-alpine");
 
     private static final AtomicBoolean serverStarted = new AtomicBoolean(false);
+
+    private static final CustomLogger logger;
+
+    static {
+        CustomLoggerFactory.initialize(Slf4jLoggerAdapter::new);
+        logger = CustomLoggerFactory.getLogger(E2EBaseConfig.class);
+    }
 
     @BeforeAll
     static void setup() {
@@ -43,5 +56,26 @@ public abstract class E2EBaseConfig {
 
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 3000;
+    }
+
+    @AfterAll
+    static void cleanDatabaseState() {
+        logger.info("Cleaning database...");
+        String truncateSql = "TRUNCATE TABLE tb_loan, tb_book, tb_customer RESTART IDENTITY CASCADE";
+
+        String jdbcUrl = postgres.getJdbcUrl();
+        String dbUser = postgres.getUsername();
+        String dbPassword = postgres.getPassword();
+
+        try (var conn = DriverManager.getConnection(jdbcUrl, dbUser, dbPassword);
+             var stmt = conn.createStatement()) {
+
+            stmt.executeUpdate(truncateSql);
+
+            logger.info("Database truncated successfully");
+
+        } catch (SQLException e) {
+            logger.error("Failed to execute truncate on the database: " + e.getMessage(), e);
+        }
     }
 }
